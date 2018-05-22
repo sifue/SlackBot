f33fd972d9a1ab4bdcc4a70635f010fa5a0c8b41
KomeijiKaede <KomeijiKoishi200058@gmail.com>
2018-05-22 14:51:15 +0900

    :thinking:

--- src/main/java/core/Bot.java
+++ src/main/java/core/Bot.java
@@ -1,18 +1,29 @@
 package core;
 
+import org.riversun.slacklet.Slacklet;
+import org.riversun.slacklet.SlackletRequest;
+import org.riversun.slacklet.SlackletResponse;
 import org.riversun.slacklet.SlackletService;
 import java.io.IOException;
+import java.util.ResourceBundle;
+
 import static core.Main.curriculum;
 import static core.Main.time;
 
 class Bot {
     static void bot() throws IOException {
-        String token = "token";
-        String Channel = "general";
+        String token = ResourceBundle.getBundle("credentials").getString("slack.bot_api_token");
         SlackletService slack = new SlackletService(token);
+        slack.addSlacklet(new Slacklet(){
+            @Override
+            public void onMentionedMessagePosted(SlackletRequest req,SlackletResponse resp){
+                String mention = req.getUserDisp();
+                resp.reply(mention + "さん、次の授業は" + curriculum(time()) + "です");
+            }
+
+        });
         slack.start();
-        slack.sendMessageTo(Channel,curriculum(time()));
-        slack.stop();
+
 
     }
 }
