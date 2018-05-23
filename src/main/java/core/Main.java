9d642dd7e38ec9e611e914dd605bd8c5ffd0b735
KomeijiKaede <KomeijiKoishi200058@gmail.com>
2018-05-23 12:50:12 +0900

    :thinking:

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -14,22 +14,19 @@ public class Main {
         System.out.println(curriculum(time()));
         bot();
     }
-
     static int time(){
         int year = ldt.getYear();
         int month = ldt.getMonth().getValue();
         int dayofmonth = ldt.getDayOfMonth();
         int n = 0;
 
-        List<LocalDateTime> time = new ArrayList<>();
-        time.add(LocalDateTime.of(year,month,dayofmonth,9,45));
+        List<LocalDateTime> nexttime = new ArrayList<>();
+        nexttime.add((LocalDateTime.of(year,month,dayofmonth,9,45)));
         for (int i=0; i<7; i++){
-            time.add(time.get(n).plusMinutes(50));
-            n++;
-            time.add(time.get(n).plusMinutes(10));
+            nexttime.add(nexttime.get(n).plusMinutes(60));
             n++;
             if (i==3){
-                time.add(time.get(5).plusMinutes(40));
+                nexttime.add(LocalDateTime.of(year,month,dayofmonth,13,15));
                 n++;
             }
         }
@@ -37,8 +34,8 @@ public class Main {
         int x = 0;
         int y = 1;
 
-        for (int i=0; i<12; i++) {
-            if (ldt.isAfter(time.get(x)) && ldt.isBefore(time.get(y))) {
+        for (int i=0; i<7; i++) {
+            if (ldt.isAfter(nexttime.get(x)) && ldt.isBefore(nexttime.get(y))) {
                 num = i;
                 break;
             } else {
@@ -46,7 +43,6 @@ public class Main {
                 y++;
             }
         }
-
         return num;
     }
 
@@ -58,81 +54,58 @@ public class Main {
         String PR ="プログラミング";
         String UJ ="大学受験/中学復習";
         String SN ="進路指導";
-        String BR ="休憩";
         String A = "";
 
         List<String> basic = new ArrayList<>();
-        for (int i=0; i<2; i++){
-            basic.add(PN);
-            basic.add(BR);
-        }
+        basic.add(PN);
+        basic.add(PN);
         basic.add(BP);
-        basic.add(BR);
-
         num++;
-
         int dow = ldt.getDayOfWeek().getValue();
         switch (dow){
             case 1:
                 List<String> mon = new ArrayList<>();
                 mon.add(EN);
-                mon.add(BR);
                 mon.add(EN);
-                mon.add(BR);
                 mon.add(PR);
-                mon.add(BR);
-                mon.add(BR);
                 basic.addAll(mon);
                 A = basic.get(num);
                 break;
             case 2:
                 List<String> tue = new ArrayList<>();
-                tue.add(CN+"/"+EN);
-                tue.add(BR);
-                tue.add(CN+"/"+PR);
-                tue.add(BR);
+                tue.add(CN + "/" + EN);
+                tue.add(CN + "/" + PR);
                 tue.add(UJ);
-                tue.add(BR);
-                tue.add(BR);
                 basic.addAll(tue);
                 A = basic.get(num);
                 break;
             case 3:
                 List<String> wed = new ArrayList<>();
                 for (int i=0; i<3; i++){
-                    wed.add(UJ+"/"+EN);
-                    wed.add(BR);
-                }
-                wed.add(BR);
+                    wed.add(UJ);
+            }
                 basic.addAll(wed);
                 A = basic.get(num);
                 break;
             case 4:
                 List<String> thu = new ArrayList<>();
-                thu.add(UJ+"/"+BP);
-                thu.add(BR);
+                thu.add(UJ + "/" + BP);
                 thu.add(EN);
-                thu.add(BR);
                 thu.add(PR);
-                thu.add(BR);
-                thu.add(BR);
                 basic.addAll(thu);
                 A = basic.get(num);
                 break;
             case 5:
                 List<String> fri = new ArrayList<>();
                 for (int i=0; i<2; i++){
-                    fri.add(CN+"/"+PR);
-                    fri.add(BR);
+                    fri.add(CN + "/" + PR);
                 }
                 fri.add(SN);
-                fri.add(BR);
-                fri.add(BR);
                 basic.addAll(fri);
                 A = basic.get(num);
                 break;
+
         }
         return A;
-
     }
 }
